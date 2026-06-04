794f31c39a81836e431f1aff93ae9cb9bc1fc263
Devanshu Singh <devasingh@cotality.com>
2026-06-04 17:52:21 +0530

    Hosting on Azure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://localhost:7142'
+  apiBaseUrl: 'https://tc-lms-backend-d3edc3h5h7b8ctg5.centralindia-01.azurewebsites.net'
 };
